93b644145f34bff47d8efc06bac271e7e572b0a2
David Mignot <david.mig@gmail.com>
2011-07-30 11:55:09 +0200

    add setRotation() method for future implementation of stars (not tested)

--- src/com/idflood/sky/SkyBillboardItem.java
+++ src/com/idflood/sky/SkyBillboardItem.java
@@ -1,24 +1,17 @@
-/*
- * To change this template, choose Tools | Templates and open the template in
- * the editor.
- */
 package com.idflood.sky;
 
 import com.jme3.asset.AssetManager;
 import com.jme3.material.Material;
 import com.jme3.material.RenderState.BlendMode;
+import com.jme3.math.Quaternion;
 import com.jme3.renderer.queue.RenderQueue.Bucket;
 import com.jme3.scene.Geometry;
 import com.jme3.scene.control.BillboardControl;
 import com.jme3.scene.shape.Quad;
 
-/**
- *
- * @author davidmignot
- */
 public class SkyBillboardItem extends Geometry{
     private static BillboardControl billBoadControl = new BillboardControl();
-    
+    private static BillboardControl billBoadControlAxis = new BillboardControl();
     private Quad quad;
     
     public SkyBillboardItem(AssetManager assetManager, String name, String texture, Float scale){
@@ -37,5 +30,10 @@ public class SkyBillboardItem extends Geometry{
         setMaterial(sunDiscMat);
         
         addControl(billBoadControl);
+    }
+    
+    public void setRotation(Float rotation){
+        billBoadControlAxis.setAlignment(BillboardControl.Alignment.AxialZ);
+        this.rotate(new Quaternion().fromAngles(0, 0, rotation));
     }
 }
